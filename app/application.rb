d6ee1a345eb9095be5c5120be695753ae3ca13cb
jennafritz <jennafritz24@gmail.com>
2021-05-27 11:34:21 -0400

    commit tree 8d57a0454d73cb0690fab0f5a6897b884c52211e

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class Application
+
+    def call(env)
+        resp = Rack::Response.new
+        current_time = Time.now.strftime("%R")
+        if current_time < "12:00"
+            resp.write "Good Morning!"
+        else
+            resp.write "Good Afternoon!"
+        end
+        resp.finish
+    end
+end
